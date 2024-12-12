62c100444ed7d941b37c605731af9d1d07c8b399
ayechan <ayechan.sant@gmail.com>
2024-12-12 14:22:01 -0800

    outline of Chance cards implemented on board

--- src/models/board.rs
+++ src/models/board.rs
@@ -1,3 +1,5 @@
+use crate::models::cards::chance::Chance;
+
 use super::{
     player::Player,
     spaces::{
@@ -29,121 +31,121 @@ impl Board {
     pub fn new() -> Self {
         let mut spaces = Vec::new();
         // Bottom 0-9
-        spaces.push(Rc::new(RefCell::new(Space::Go)));
+        spaces.push(Rc::new(RefCell::new(Space::Go))); // 0
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Brown(
                 BrownProperty::mediterranean_ave(),
             )),
-        ))));
-        spaces.push(Rc::new(RefCell::new(Space::CommunityChest)));
+        )))); // 1
+        spaces.push(Rc::new(RefCell::new(Space::CommunityChest))); // 2
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Brown(BrownProperty::baltic_ave())),
-        ))));
-        spaces.push(Rc::new(RefCell::new(Space::IncomeTax)));
+        )))); // 3
+        spaces.push(Rc::new(RefCell::new(Space::IncomeTax))); // 4
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::Railroad(Railroads::reading()),
-        ))));
+        )))); // 5
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::LightBlue(
                 LightBlueProperty::oriental_ave(),
             )),
-        ))));
-        spaces.push(Rc::new(RefCell::new(Space::Chance)));
+        )))); // 6
+        spaces.push(Rc::new(RefCell::new(Space::Chance))); // 7
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::LightBlue(
                 LightBlueProperty::vermont_ave(),
             )),
-        ))));
+        )))); // 8
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::LightBlue(
                 LightBlueProperty::connecticut_ave(),
             )),
-        ))));
-        // Left 10-19
+        )))); // 9
+              // Left 10-19
         spaces.push(Rc::new(RefCell::new(Space::Jail))); // 10
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Pink(PinkProperty::st_charles_place())),
-        ))));
+        )))); // 11
         spaces.push(Rc::new(RefCell::new(Space::Property(Properties::Utility(
             Utilities::electric_company(),
-        )))));
+        ))))); // 12
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Pink(PinkProperty::states_ave())),
-        ))));
+        )))); // 13
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Pink(PinkProperty::virginia_ave())),
-        ))));
+        )))); // 14
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::Railroad(Railroads::pennsylvania()),
-        ))));
+        )))); // 15
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(
                 ColoredProperties::Orange(OrangeProperty::st_james_place()),
             ),
-        ))));
-        spaces.push(Rc::new(RefCell::new(Space::CommunityChest)));
+        )))); // 16
+        spaces.push(Rc::new(RefCell::new(Space::CommunityChest))); // 17
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Orange(OrangeProperty::tennessee_ave())),
-        ))));
+        )))); // 18
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Orange(OrangeProperty::new_york_ave())),
-        ))));
-        // Top 20-29
+        )))); // 19
+              // Top 20-29
         spaces.push(Rc::new(RefCell::new(Space::FreeParking))); // 20
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Red(RedProperty::kentucky_ave())),
-        ))));
+        )))); // 21
         spaces.push(Rc::new(RefCell::new(Space::Chance))); // 22
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Red(RedProperty::indiana_ave())),
-        ))));
+        )))); // 23
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Red(RedProperty::illinois_ave())),
-        ))));
+        )))); // 24
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::Railroad(Railroads::bo()),
-        ))));
+        )))); // 25
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Yellow(YellowProperty::atlantic_ave())),
-        ))));
+        )))); // 26
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Yellow(YellowProperty::ventnor_ave())),
-        ))));
+        )))); // 27
         spaces.push(Rc::new(RefCell::new(Space::Property(Properties::Utility(
             Utilities::water_works(),
-        )))));
+        ))))); // 28
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(
                 ColoredProperties::Yellow(YellowProperty::marvin_gardens()),
             ),
-        ))));
-        // Right 30-39
-        spaces.push(Rc::new(RefCell::new(Space::GoToJail)));
+        )))); // 29
+              // Right 30-39
+        spaces.push(Rc::new(RefCell::new(Space::GoToJail))); // 30
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Green(GreenProperty::pacific_ave())),
-        ))));
+        )))); // 31
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Green(
                 GreenProperty::north_carolina_ave(),
             )),
-        ))));
-        spaces.push(Rc::new(RefCell::new(Space::CommunityChest)));
+        )))); // 32
+        spaces.push(Rc::new(RefCell::new(Space::CommunityChest))); // 33
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(
                 ColoredProperties::Green(GreenProperty::pennsylvania_ave()),
             ),
-        ))));
+        )))); // 34
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::Railroad(Railroads::shortline()),
-        ))));
-        spaces.push(Rc::new(RefCell::new(Space::Chance)));
+        )))); // 35
+        spaces.push(Rc::new(RefCell::new(Space::Chance))); // 36
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Blue(BlueProperty::park_place())),
-        ))));
-        spaces.push(Rc::new(RefCell::new(Space::LuxuryTax)));
+        )))); // 37
+        spaces.push(Rc::new(RefCell::new(Space::LuxuryTax))); // 38
         spaces.push(Rc::new(RefCell::new(Space::Property(
             Properties::ColoredProperty(ColoredProperties::Blue(BlueProperty::boardwalk())),
-        ))));
+        )))); // 39
 
         let players = vec![
             Rc::new(RefCell::new(Player::new(1))),
@@ -196,6 +198,8 @@ impl Board {
                 }
             }
             Space::Chance => {
+                let chance_card = Chance::random_card();
+                chance_card.execute_card(player_ref.clone(), self);
                 println!(
                     "Player {:?} has landed on Chance!",
                     player_ref.borrow().player_number
--- src/models/cards/chance.rs
+++ src/models/cards/chance.rs
@@ -1,6 +1,6 @@
 use rand::Rng;
 
-use crate::models::board::PlayerRef;
+use crate::models::board::{Board, PlayerRef};
 
 pub enum Chance {
     One,
@@ -44,27 +44,103 @@ impl Chance {
             _ => unreachable!(),
         }
     }
-    pub fn execute_card(player_ref: PlayerRef) {
+    pub fn execute_card(&self, player_ref: PlayerRef, board: &Board) {
         let card = Self::random_card();
+        let mut player = player_ref.borrow_mut();
         match card {
-            Chance::One => todo!(),
-            Chance::Two => todo!(),
-            Chance::Three => todo!(),
-            Chance::Four => todo!(),
-            Chance::Five => todo!(),
-            Chance::Six => todo!(),
-            Chance::Seven => todo!(),
-            Chance::Eight => todo!(),
-            Chance::Nine => todo!(),
-            Chance::Ten => todo!(),
-            Chance::Eleven => todo!(),
-            Chance::Twelve => todo!(),
-            Chance::Thirteen => todo!(),
-            Chance::Fourteen => todo!(),
-            Chance::Fifteen => todo!(),
-            Chance::Sixteen => todo!(),
+            Chance::One => {
+                // Advance to Boardwalk
+                player.position = 39;
+            }
+            Chance::Two => {
+                // Advance to Go and collect $200
+                player.position = 0;
+            }
+            Chance::Three => {
+                // Advance to Illinois Ave
+                player.position = 24;
+            }
+            Chance::Four => {
+                // Advance to St. Charles Place
+                player.position = 11;
+            }
+            Chance::Five => {
+                // Nearest railroad
+                let player_position = player.position;
+                if player_position == 7 {
+                    player.position = 15;
+                } else if player.position == 22 {
+                    player.position = 25;
+                } else if player.position == 36 {
+                    player.position = 5;
+                } else {
+                    println!("Failed to move to nearest Railroad :(");
+                }
+            }
+            Chance::Six => {
+                // Nearest railroad
+                let player_position = player.position;
+                if player_position == 7 {
+                    player.position = 15;
+                } else if player.position == 22 {
+                    player.position = 25;
+                } else if player.position == 36 {
+                    player.position = 5;
+                } else {
+                    println!("Failed to move to nearest Railroad :(");
+                }
+            }
+            Chance::Seven => {
+                // Nearest Utility
+                let player_position = player.position;
+                if player_position == 7 || player.position == 36 {
+                    player.position = 12;
+                } else if player.position == 28 {
+                    player.position = 25;
+                } else {
+                    println!("Failed to move to nearest Railroad :(");
+                }
+            }
+            Chance::Eight => {
+                // Bank pays you $50
+                player.money += 50;
+            }
+            Chance::Nine => {
+                println!("Get out of jail for free card!");
+            }
+            Chance::Ten => {
+                // Move back 3 spaces
+                player.position -= 3;
+            }
+            Chance::Eleven => {
+                // Go to Jail, no $200
+                player.position = 10;
+            }
+            Chance::Twelve => {
+                // For each house pay $25, for each hotel pay $100
+                println!("For each house pay $25, for each hotel pay $100");
+            }
+            Chance::Thirteen => {
+                // Speeding fine
+                player.money -= 15;
+            }
+            Chance::Fourteen => {
+                // Reading Railroad
+                player.position = 5;
+            }
+            Chance::Fifteen => {
+                // Pay each player $50
+                let players = &board.players;
+                for other_player_ref in players.iter() {
+                    let mut other_player = other_player_ref.borrow_mut();
+                    other_player.money += 50;
+                }
+                player.money -= 200;
+            }
+            Chance::Sixteen => {
+                // Collect $150
+                player.money += 150;
+            }
         }
     }
 }
-
-// Needs to take in a player struct, board struct
