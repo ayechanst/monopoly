4fd5ac24d9e6473c9dcee553dbc5d0dba82f9894
ayechan <ayechan.sant@gmail.com>
2025-01-20 12:54:30 -0800

    in the middle of connecting mpsc

--- src/bevy_logic/world.rs
+++ src/bevy_logic/world.rs
@@ -1,21 +1,22 @@
+use super::buttons::buttons;
 use crate::{bevy_logic::sprite_builder::make_space::make_space, models::board::Board};
 use bevy::prelude::*;
 use bevy_egui::EguiPlugin;
 
-use super::buttons::buttons;
 pub struct WorldPlugin;
 
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(EguiPlugin)
-            .add_systems(Startup, (spawn_camera, spawn_light))
-            .add_systems(Update, buttons);
+            .add_systems(Startup, (spawn_camera, spawn_light, spawn_board));
+        // .add_systems(Update, buttons);
         // .add_systems(Update, buttons);
     }
 }
 
+// this should take in board from the front-end receiver
 pub fn spawn_board(mut commands: Commands) {
-    let board = Board::new();
+    // let board = Board::new();
     let grid_size = 600.0;
     let scale_factor = 1.0;
     for (index, space_ref) in board.spaces.iter().cloned().enumerate() {
--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use std::{
     io::{self, Write},
     sync::mpsc,
 };
+use utils::cmd_consumer::backend_loop;
 mod utils;
-
 use bevy::prelude::*;
 pub enum Command {
     SpawnBoard,
@@ -21,11 +21,21 @@ pub enum Command {
 }
 
 fn main() {
+    // creates a channel for front end to communicate with backend
+    // commands received from front-end
     let (command_tx, command_rx) = mpsc::channel::<Command>();
+    // creates a channel for back end to communicate with frontend
+    // data being sent back to front-end
+    let (update_tx, update_rx) = mpsc::channel::<Command>();
+
+    std::thread::spawn(move || {
+        let mut board = Board::new();
+        backend_loop(&mut board, command_rx, update_tx);
+    });
 
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(WorldPlugin)
+        // .add_plugins(WorldPlugin)
         .run();
 }
 
@@ -38,18 +48,6 @@ fn main() {
 //         board.first_main_phase(current_player);
 //         round_count += 1;
 //         // wait_for_player();
-//     }
-// }
-
-// fn main() {
-//     let mut board = Board::new();
-//     let mut round_count = 0;
-//     let num_of_players = 4;
-//     loop {
-//         let current_player = round_count % num_of_players;
-//         board.first_main_phase(current_player);
-//         round_count += 1;
-//         wait_for_player();
 //     }
 // }
 
